6b866321bb7efc4d919c2a44704f748c6aa89eb1
childevnote <daniel011125@gmail.com>
2023-08-30 12:16:13 +0900

    [update] 수정 기능 추가

--- app.js
+++ app.js
@@ -1,58 +1,69 @@
 const $addTodo = document.querySelector('#add-todo');
 const $todoList = document.querySelector('.todo-list');
-
-const $newTodo = document.createElement('li');
-const $checkbox = document.createElement('input');
-const $todoText = document.createElement('div');
-const $paragraph = document.createElement('p');
-const $buttons = document.createElement('div');
-const $editButton = document.createElement('button');
-const $editImg = document.createElement('img');
-const $deleteButton = document.createElement('button');
-const $deleteImg = document.createElement('img');
+const $clearTodo = document.querySelector('#clear-todo');
 
 let todoList = [];
 
 loadTodoList();
 
-function createTodoItem(todoContent, completed, index) {
+function createTodoItem(todoContent, index) {
+  const $newTodo = document.createElement('li');
   $newTodo.className = 'todo-item';
 
+  const $checkbox = document.createElement('input');
   $checkbox.type = 'checkbox';
-  $checkbox.id = 'checkbox';
+  $checkbox.id = 'checkbox-' + index; 
+  $checkbox.className = 'checkbox';
+
   $checkbox.addEventListener('change', () => {
     toggleComplete(index);
   });
 
+  const $todoText = document.createElement('div');
   $todoText.className = 'todo';
+  const $paragraph = document.createElement('p');
   $paragraph.textContent = todoContent;
   $todoText.appendChild($paragraph);
+
+  const $buttons = document.createElement('div');
   $buttons.className = 'todo-item-buttons';
+
+  const $editButton = document.createElement('button');
   $editButton.className = 'edit-button';
+  const $editImg = document.createElement('img');
   $editImg.src = 'assets/icons/pencil.svg';
   $editImg.alt = 'edit button';
   $editButton.appendChild($editImg);
+  $editButton.addEventListener('click', () => {
+    editTodo(index);
+  });
+
+  const $deleteButton = document.createElement('button');
   $deleteButton.className = 'delete-button';
+  const $deleteImg = document.createElement('img');
   $deleteImg.src = 'assets/icons/delete.svg';
   $deleteImg.alt = 'delete button';
   $deleteButton.addEventListener('click', () => {
     deleteTodo(index);
   });
-
   $deleteButton.appendChild($deleteImg);
+
   $buttons.appendChild($editButton);
   $buttons.appendChild($deleteButton);
+
   $newTodo.appendChild($checkbox);
   $newTodo.appendChild($todoText);
   $newTodo.appendChild($buttons);
 
   return $newTodo;
+  console.log("[create] new element added");
+
 }
 
 
 // 새로운 할일 추가 후 저장
 $addTodo.addEventListener('click', () => {
-  const todoContent = prompt('새로운 Todo를 추가해주세요!');
+  const todoContent = '할 일을 입력해보세요!';
 
   if (todoContent) {
     const newTodo = {
@@ -73,6 +84,7 @@ function loadTodoList() {
     todoList = JSON.parse(savedTodoList);
     renderTodoList();
   }
+  console.log("[system] todolist loaded");
 }
 
 // 할일 목록을 로컬 스토리지에 저장하는 함수
@@ -83,24 +95,63 @@ function saveTodoList() {
 // 할일 목록을 화면에 렌더링하는 함수
 function renderTodoList() {
   $todoList.innerHTML = '';
-
   todoList.forEach((todo, index) => {
-    const $newTodo = createTodoItem(todo.content, todo.completed);
+    const $newTodo = createTodoItem(todo.content, index);
     $todoList.appendChild($newTodo);
   });
+  console.log("[system] todolist rendered");
 }
 
+// 할 일 목록 삭제
 function deleteTodo(index) {
   todoList.splice(index, 1);
   saveTodoList();
   renderTodoList();
+  console.log("[delete] "+index +" element deleted")
 }
 
-// 체크박스 상태가 변경되었을 때 실행되는 함수
+$clearTodo.addEventListener('click', () => {
+  todoList=[];
+  saveTodoList();
+  renderTodoList();
+  console.log("[delete] clear all");
+});
+
+// 체크박스 상태 변경 토글
 function toggleComplete(index) {
   if (index >= 0 && index < todoList.length) {
     todoList[index].completed = !todoList[index].completed;
     saveTodoList();
     renderTodoList();
   }
+  console.log("[update] "+index+" element completed toggle changed");
+}
+
+// 할일 수정 함수
+function editTodo(index) {
+  const $todoText = $todoList.children[index].querySelector('.todo');
+  const $paragraph = $todoText.querySelector('p');
+  const $editButton = $todoList.children[index].querySelector('.edit-button');
+
+  const $input = document.createElement('input');
+  $input.type = 'text';
+  $input.className = 'todo-input';
+  $input.value = $paragraph.textContent;
+
+  $todoText.removeChild($paragraph);
+  $todoText.appendChild($input);
+  $input.focus();
+
+  $editButton.addEventListener('click', () => {
+    todoList[index].content = $input.value;
+    saveTodoList();
+    renderTodoList();
+  });
+
+  $input.addEventListener('blur', () => {
+    todoList[index].content = $input.value;
+    saveTodoList();
+    renderTodoList();
+    console.log("[update] "+index +" element edited to "+$input.value);
+  });
 }
